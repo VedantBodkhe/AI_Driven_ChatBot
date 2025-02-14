594c5d61327668c048e1e8425583a57049d2b94c
UtsavRajGupta2003 <utsavrajgupta2003@gmail.com>
2025-02-14 15:56:27 +0530

    added voice assistant

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import ChatWithGemini from './components/ChatWithGemini'
 import { Container } from '@chakra-ui/react'
+
 import './App.css'
+import VoiceToTextChat from './components/VoiceToTextChat'
 
 function App() {
 
   return (
     <Container maxW={'none'} className="App" bgColor={'black'} bgGradient={'linear(to-r, gray.800, blue.700)'} color={'black'}>
       <ChatWithGemini />
+      {/* <VoiceToTextChat/> */}
     </Container>
   )
 }
